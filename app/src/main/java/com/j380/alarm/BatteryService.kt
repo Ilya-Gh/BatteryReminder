dc879975acc4bc7d483e0116362e43414c09556a
Jez380 <ping013ilya@gmail.com>
2016-06-02 18:59:34 +0300

    BatteryService refactoring

--- app/src/main/java/com/j380/alarm/BatteryService.kt
+++ app/src/main/java/com/j380/alarm/BatteryService.kt
@@ -12,11 +12,9 @@ import com.j380.alarm.view.AlertView
 
 class BatteryService : Service() {
 
-    private var batteryPercents: Float = 0f
-
     private val LOW_BATTERY_LEVEL = 25f
 
-    private lateinit var alertView : AlertView
+    private lateinit var alertView: AlertView
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -35,38 +33,57 @@ class BatteryService : Service() {
     }
 
     fun setAlarm() {
-        val alarmManager = applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager;
+        val alarmManager = applicationContext.getSystemService(
+                Context.ALARM_SERVICE) as AlarmManager;
         val lIntent = Intent(getString(R.string.checkBatteryIntentAction));
-        val lPendingIntent = PendingIntent.getBroadcast(applicationContext, 0, lIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        val lPendingIntent = PendingIntent.getBroadcast(applicationContext, 0, lIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(),
                 AlarmManager.INTERVAL_FIFTEEN_MINUTES, lPendingIntent);
     }
 
     fun checkBattery() {
-        val lFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
-        val batteryStatus = applicationContext.registerReceiver(null, lFilter)
+        val batteryStatus = getBatteryStatusIntent()
 
-        val level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
-        val scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
-
-        if (level != -1 && scale != -1) {
-            batteryPercents = level / scale.toFloat();
-            batteryPercents *= 100;
+        if(isNotPluggedIn(batteryStatus)) {
+            val batteryLevel = convertBatteryLevelToPercent(getRawBatteryLevel(batteryStatus),
+                    getBatteryScale(batteryStatus))
+            showViewIfBatteryLevelIsLow(batteryLevel)
         }
-
-        val chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1)
-        val usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB
-        val acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC
-
-        if (batteryPercents <= LOW_BATTERY_LEVEL && !usbCharge && !acCharge) {
-            alertView.show(batteryPercents);
-        }
-
     }
 
+    fun getBatteryStatusIntent(): Intent {
+        val lFilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+        return applicationContext.registerReceiver(null, lFilter)
+    }
 
+    fun getRawBatteryLevel(batteryStatus: Intent) = batteryStatus.getIntExtra(
+            BatteryManager.EXTRA_LEVEL, -1)
 
+    fun getBatteryScale(batteryStatus: Intent) = batteryStatus.getIntExtra(
+            BatteryManager.EXTRA_SCALE, -1)
 
+    fun convertBatteryLevelToPercent(rawLevel: Int, scale: Int): Float {
+        return rawLevel / scale.toFloat() * 100;
+    }
 
+    fun getTypeOfChargePlug(batteryStatus: Intent) = batteryStatus.getIntExtra(
+            BatteryManager.EXTRA_PLUGGED, -1)
 
+    fun isNotPluggedIn(batteryStatus: Intent) = !isPluggedInUsbCharger(batteryStatus) &&
+            !isPluggedInAcCharger(batteryStatus)
+
+    fun isPluggedInUsbCharger(batteryStatus: Intent): Boolean {
+        return getTypeOfChargePlug(batteryStatus) == BatteryManager.BATTERY_PLUGGED_USB
+    }
+
+    fun isPluggedInAcCharger(batteryStatus: Intent): Boolean {
+        return getTypeOfChargePlug(batteryStatus) == BatteryManager.BATTERY_PLUGGED_AC
+    }
+
+    fun showViewIfBatteryLevelIsLow(batteryLevel: Float) {
+        if (batteryLevel <= LOW_BATTERY_LEVEL ) {
+            alertView.show(batteryLevel);
+        }
+    }
 }
